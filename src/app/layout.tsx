7dde11e3d432e3123d53a157222c4d813a372923
Braulio Fernandes <brauliopf+github@gmail.com>
2025-01-21 21:48:44 -0500

    install shadcn + add sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs"; // https://clerk.com/docs/quickstarts/nextjs#add-clerk-provider-and-clerk-components-to-your-app
 import "./globals.css";
-import Sidebar from "./components/Sidebar";
+import AppSidebar from "./_components/Sidebar";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 
 export default function RootLayout({
   children,
@@ -14,12 +15,13 @@ export default function RootLayout({
           <meta name="viewport" content="..." />
         </head>
         <body>
-          <div className="flex">
-            <header>
-              <Sidebar />
-            </header>
-            <main>{children}</main>
-          </div>
+          <SidebarProvider defaultOpen={false}>
+            <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>
         </body>
       </html>
     </ClerkProvider>
