110885416161ae974303017e21dad7146d2ee117
Braulio Fernandes <brauliopf+github@gmail.com>
2025-02-09 21:53:21 -0500

    delete unused routes page

--- src/app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-// import { getUsers } from "@/lib/db/query";
-
-// export async function GET(request: Request) {
-//   const resp = await getUsers();
-//   return new Response(JSON.stringify(resp), {
-//     status: 200,
-//   });
-// }
