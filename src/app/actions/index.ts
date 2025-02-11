b0454397a5ad6e22b4beae821d4242eeb98d4a32
Braulio Fernandes <brauliopf+github@gmail.com>
2025-02-11 00:25:24 -0500

    get the sandbox page back and refactor the video process script

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -2,7 +2,6 @@
 
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { generateContent, getTextEmbedding } from "@/lib/gemini";
-import { VideoIDKey } from "@/types/video";
 import { getFilesFromS3 } from "@/lib/s3";
 import { updateVideoMetadata } from "@/db/mutations";
 
@@ -49,55 +48,16 @@ export const generateVideoMetadata = async ({
   }
 
   // get summary embeddings
-  // const textEmbeddings = await getTextEmbedding(
-  //   `${summary}. Interests: ${[interests].join(", ")}`
-  // )
-
-  // store video metadata
-  // console.log(`${summary}. Interests: ${[interests].join(",")}`);
-  await updateVideoMetadata({
-    id: id,
-    summary,
-    interests: Array.isArray(interests) ? interests : [interests],
-    // embeddings: textEmbeddings,
-  });
-};
-
-export const generateVideoMetadataSandbox = async (formData: FormData) => {
-  const videoString = formData.get("video"); // id, s3Key, title, status
-
-  if (typeof videoString !== "string") {
-    throw new Error("No video JSON data found in FormData");
-  }
-
-  // get video summary
-  const video: VideoIDKey = JSON.parse(videoString);
-  let summary = "";
-  let interests = "";
-  let url = "";
-  try {
-    const s3Videos = await getFilesFromS3([video.s3Key]);
-    const content = (await generateContent(s3Videos[0].url)) || {};
-    summary = content.summary;
-    interests = content.interests;
-    url = s3Videos[0].url;
-  } catch (e) {
-    console.log("Error processing video", e);
-  }
-
-  // get summary embeddings
   const textEmbeddings = await getTextEmbedding(
     `${summary}. Interests: ${[interests].join(", ")}`
   );
 
   // store video metadata
-  // console.log(`${summary}. Interests: ${[interests].join(",")}`);
+  console.log(`${summary}. Interests: ${[interests].join(",")}`);
   await updateVideoMetadata({
-    id: video.id,
+    id: id,
     summary,
     interests: Array.isArray(interests) ? interests : [interests],
     embeddings: textEmbeddings,
   });
-
-  console.log("UPDATED METADATA", summary, textEmbeddings.length, url);
 };
---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,56 @@
+import { getVideos } from "@/db/query";
+import { generateVideoMetadata } from "../actions";
+import { VideoIDKey } from "@/types/video";
+
+export default async function Sandbox() {
+  const videos = await getVideos({ limit: 10, offset: 0 });
+
+  const testAction = async (formData: FormData) => {
+    "use server";
+    const videoString = formData.get("video"); // id, s3Key, title, status
+
+    if (typeof videoString !== "string") {
+      throw new Error("No video JSON data found in FormData");
+    }
+
+    const video: VideoIDKey = JSON.parse(videoString);
+    const id = video.id;
+    const s3Key = video.s3Key;
+
+    await generateVideoMetadata({ id, s3Key });
+  };
+
+  return (
+    <div>
+      <h1>Sandbox</h1>
+      <div className="align-center p-10 w-1/2">
+        <h2>Video Processing</h2>
+        <form
+          className=" flex flex-col gap-4 border-gray-300"
+          action={testAction}
+        >
+          <select
+            name="video"
+            className="p-2.5 border border-gray-300 rounded-md w-full"
+          >
+            {videos.data.map((video) => (
+              <option
+                value={JSON.stringify(video)}
+                key={video.title}
+                style={{ padding: "10px" }}
+              >
+                {video.title}
+              </option>
+            ))}
+          </select>
+          <button
+            type="submit"
+            className="px-5 py-2 rounded-md bg-blue-500 text-white cursor-pointer"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
