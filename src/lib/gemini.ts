2e4c57132edb16e40a4da4796672757d9c8748f8
Braulio Fernandes <brauliopf+github@gmail.com>
2025-02-11 14:12:53 -0500

    fix typo on gemini.ts

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -12,7 +12,8 @@ export async function generateMetadata(fileUrl: string) {
     const vertexAI = new VertexAI({
       project: "headstarter-441420",
       googleAuthOptions: {
-        keyFilename: process.env.GOOGLE_AUTH_OPTION_SERVICEACC
+        keyFilename: process.env.GOOGLE_AUTH_OPTION_SERVICEACC,
+      },
     });
     const generativeModel = vertexAI.getGenerativeModel({
       model: "gemini-1.5-flash-001",
